d4fcc65dcaa1d2dafa6a72d29dedaf47c4815da6
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2022-03-05 15:22:15 -0600

    Add files via upload

--- Storage.java
+++ Storage.java
@@ -2,17 +2,9 @@
 Clase que relaciona Stacks y Listas, puesto que factory regresa la clase correspondiente
 */
 public class Storage<T>  {
-    protected String type;
-
-    public Storage(String type) {
-        this.type = type;
-    }
-
-    public String getType() {//servira para ver que tipo es, 
-        return type;
-    }
     public Class me(){//regresa la clase que es.
         return this.getClass();
+        
     }
     
 }
