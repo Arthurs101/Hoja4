f0c7eba2db2aa4e5efa6e3205a0ec112e4cd7218
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2022-03-06 17:50:09 -0600

    Add files via upload

--- StorageFactory.java
+++ StorageFactory.java
@@ -1,26 +1,34 @@
 
-
 /**
  *
  * @author inmar
  */
 public class StorageFactory {
-    public Storage createInstance(int type){
-    switch(type){
-        case 1 ->{//lista simple
-        return new SimpleList();
+    public IStack createInstance(int type) {
+        switch (type) {
+     
+            case 1:// stack simple con array list
+                return new StackUsingArrayList<Integer>();
+            // break;
+            case 2:// Stack con lista enlazada simple
+                return new StackUsingLinkedList<Integer>();
+
+            case 3:// stack con lista enlazada doble
+                return new StackUsingDoubleLinkedList<Integer>();
+
         }
-        case 2 ->{//lista doble anidada
-        return new DoubleList();
-        }
-        case 3 ->{//stack simple con array list
-        return new StackList();}
-        case 4 ->{//Stack con lista enlazada simple
-        return new StackSimple();}
-        case 5 ->{//stack con lista enlazada doble
-        return new StackDouble();
-        }
-    }
         return null;
     }
+    public IList createList(int type){
+    switch(type){
+        case 1:// lista simple
+        return new SingleLinkedList<Integer>();
+
+        case 2:// lista doble anidada
+        return new DoubleLinkedList<Integer>();
+    }
+    
+    return null;
+    }
+   
 }
